a4e0b72c8f2740872b8a4cd12c94af1ea1ff86ca
cmmgw <cecily.martin@gmail.com>
2021-03-06 17:06:21 -0500

    Adding Simple_Map files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -6,23 +6,13 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // // Create the map object with a center and zoom level.
 // // Alternate way to write the code above
-// This method is useful when we need to add multiple tile layers, or a background image of our map(s)
+// This method is useful when needing to add multiple tile layers, or a background image of our map(s)
 // let map = L.map("mapid", {
 //     center: [
 //       40.7, -94.5
 //     ],
 //     zoom: 4
 //   });
-
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
