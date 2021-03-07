248d49b794a00cd7a03730ebdfc60b2d8d168596
cecily <cecily.martin@gmail.com>
2021-03-07 13:54:25 -0500

    Adding Mapping_Lines/ files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -18,8 +18,8 @@
 //     color: "yellow"
 //  }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
-// Satellite Background
+// // We create the tile layer that will be the background of our map.
+// // Satellite Background
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 // attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
