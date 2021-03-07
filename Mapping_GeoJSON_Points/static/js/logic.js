451f8d414ed0f67ce67892030aad81615ab0e7b0
cecily <cecily.martin@gmail.com>
2021-03-07 16:53:49 -0500

    Adding Mapping_GeoJSON_Points files

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -174,61 +174,97 @@ d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data) 
-layer.bindPopup("<h2>" + "Airport: " + feature.properties.faa + "</h2>" + "<h2>" + "Airport name: " + feature.properties.name +"</h2>");
+L.bindPopup("<h2>" + "Airport: " + feature.properties.faa + "</h2>" + "<h2>" + "Airport name: " + feature.properties.name +"</h2>");
 }).addTo(map);
 
 
-// // L.geoJson(sanFranAirport, {
-// //   // We turn each feature into a marker on the map.
-// //     onEachFeature: function(feature, layer) {
-// //       console.log(layer);
-//       layer.bindPopup("<h2>" + "Airport: " + feature.properties.faa + "</h2>" + "<h2>" + "Airport name: " + feature.properties.name +"</h2>");
-//  }
-// }).addTo(map);
+
 
 
 
 
 // 13.5.4 Add Multiple Maps
 // Create the street view tile layer that will be the default background for map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Create the dark view tile layer that will be an option for map.
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Create a base layer that holds both maps.
+// let baseMaps = {
+//   Street: streets,
+//   Dark: dark
+// };
+
+// // Create the map object with center, zoom level and default layer.
+// let map = L.map('mapid', {
+//   center: [30, 30],
+//   zoom: 2,
+//   layers: [streets]
+// })
+
+// // Pass our map layers into our layers control and add the layers control to the map.
+// L.control.layers(baseMaps).addTo(map);
+
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/cmmgw/Mapping_Earthquakes/main/majorAirports.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data).addTo(map);
+// });
 
 
-// Create the dark view tile layer that will be an option for map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
 
+// // 13.5.4 Add Multiple Maps SKILL DRILL
+// // Edit your L.geoJson() layer to add a popup marker that displays all airports' codes and names for both the Street and Dark layers.
 
+// // Create the street view tile layer that will be the default background for map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
 
+// // Create the dark view tile layer that will be an option for map.
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
 
+// // Create a base layer that holds both maps.
+// let baseMaps = {
+//   Street: streets,
+//   Dark: dark
+// };
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// // Create the map object with center, zoom level and default layer.
+// let map = L.map('mapid', {
+//   center: [30, 30],
+//   zoom: 2,
+//   layers: [streets]
+// })
 
-// We create the tile layer that will be the background of our map.
-// // Streets Background
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// // Pass our map layers into our layers control and add the layers control to the map.
+// L.control.layers(baseMaps).addTo(map);
 
-// Then we add our 'streets' tile layer to the map.
-streets.addTo(map);
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/cmmgw/Mapping_Earthquakes/main/majorAirports.json";
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/cmmgw/Mapping_Earthquakes/main/majorAirports.json";
-
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
-});
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data).addTo(map);
+// });
