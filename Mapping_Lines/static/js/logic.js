f5c13ebcd91248bf6025dbde59fbce339fe2e059
cecily <cecily.martin@gmail.com>
2021-03-07 04:29:03 -0500

    Adding Mapping_Lines/ files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,20 +1,62 @@
+// // Add console.log to check to see if our code is working.
+// console.log("working");
+
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// // Coordinates for each point to be used in the polyline.
+// // LAX - SFO - SLC - SEA
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+// // Create a polyline using the line coordinates and make the line yellow.
+// L.polyline(line, {
+//     color: "yellow"
+//  }).addTo(map);
+
+// // We create the tile layer that will be the background of our map.
+// // Dark Background
+// // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// // Default Background
+// // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// // Satellite Background
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+
+//     accessToken: API_KEY
+// });
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+
+// SKILL DRILL 
 // Add console.log to check to see if our code is working.
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.6213, -122.3790], 4);
 
 // Coordinates for each point to be used in the polyline.
+// SFO - AUS - YYZ - DCA - SFO
 let line = [
-    [33.9416, -118.4085],
     [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [38.8512, -77.0402],
+    [40.6413, -73.7781],
   ];
 
-// Create a polyline using the line coordinates and make the line black.
+// Create a polyline using the line coordinates and make the line blue and dashed.
 L.polyline(line, {
-    color: "yellow"
+    color: "blue",
+    opacity: 0.5,
+    dashArray: '2, 5'
  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
@@ -23,7 +65,9 @@ L.polyline(line, {
 // Default Background
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 // Satellite Background
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Light Background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
 
@@ -31,4 +75,4 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+streets.addTo(map); 
