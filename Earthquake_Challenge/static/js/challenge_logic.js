9ac5e2576a5ec5c9e2cf10be6cbb9b0fccad1991
cecily <cecily.martin@gmail.com>
2021-03-13 11:38:34 -0500

    adding challenge_logic.js

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -28,7 +28,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 
 
 // Extra
-let dayPreview = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
@@ -55,7 +55,7 @@ let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets,
   // "Dark": dark
-  "Day Preview": dayPreview,
+  "Outdoors": outdoors,
  "Night Preview": nightPreview
 };
 
